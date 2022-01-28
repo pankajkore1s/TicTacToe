f07758c097c01d31b634fb48c711342652f1791e
pankaj kore <korep95@gmail.com>
2022-01-28 13:27:25 +0530

    [pankaj]created the board

--- src/com/tictactoe/tictactoe.java
+++ src/com/tictactoe/tictactoe.java
@@ -1,5 +1,16 @@
 package com.tictactoe;
 
 public class tictactoe {
+	static char[] board = new char[10];
 
+	static void createBoard(){
+		for(int i=0;i<10;i++){
+			board[i]=' ';
+		}
+	}
+
+	public static void main(String[] args){
+		System.out.println("----- Welcome To The Game Of Tic Tac Toe -----");
+		createBoard();
+	}
 }
