c7088679c7a1262c23f9f080218ef1c788d85d21
pankaj kore <korep95@gmail.com>
2022-01-28 13:43:31 +0530

    [pankaj]added method to get players choice

--- src/com/tictactoe/tictactoe.java
+++ src/com/tictactoe/tictactoe.java
@@ -1,7 +1,11 @@
 package com.tictactoe;
 
+import java.util.*;
+
 public class tictactoe {
 	static char[] board = new char[10];
+	static char player,computer;
+	public static Scanner sc=new Scanner(System.in);
 
 	static void createBoard(){
 		for(int i=0;i<10;i++){
@@ -9,8 +13,21 @@ public class tictactoe {
 		}
 	}
 
+	static void getPlayerChoice(){
+		System.out.println("select x or o:");
+		player=Character.toUpperCase(sc.next().charAt(0));
+
+		if (player=='X')
+			computer='o';
+		else
+			computer='x';
+		System.out.println("you have selected:" +player);
+		System.out.println("computer's choice is:" +computer);
+	}
+
 	public static void main(String[] args){
 		System.out.println("----- Welcome To The Game Of Tic Tac Toe -----");
 		createBoard();
+		getPlayerChoice();
 	}
 }
